916a47b52d36117feeae7499668c4e007fcf4869
V0JT48 <46529036+V0JT48@users.noreply.github.com>
2024-08-17 18:21:00 +0200

    Added ON state pin

--- components/mppt3000pro/mppt3000pro.h
+++ components/mppt3000pro/mppt3000pro.h
@@ -1,14 +1,16 @@
 #pragma once
 
 #include "esphome/core/component.h"
+#include "esphome/core/hal.h"
 #include "esphome/components/sensor/sensor.h"
+#include "esphome/components/binary_sensor/binary_sensor.h"
 #include "Wire.h"
 
 
 namespace esphome {
 namespace mppt3000pro {
 
-class MPPT3000PRO : public sensor::Sensor, public PollingComponent {
+class MPPT3000PRO : public sensor::Sensor, public binary_sensor::BinarySensor, public PollingComponent {
   public:  
     void set_sensor_V(sensor::Sensor *sensor_V) { sensor_V_ = sensor_V; }
     void set_sensor_A(sensor::Sensor *sensor_A) { sensor_A_ = sensor_A; }
@@ -16,9 +18,12 @@ class MPPT3000PRO : public sensor::Sensor, public PollingComponent {
     void set_sensor_D(sensor::Sensor *sensor_D) { sensor_D_ = sensor_D; }
     void set_sensor_T(sensor::Sensor *sensor_T) { sensor_T_ = sensor_T; }
     void set_sensor_O(sensor::Sensor *sensor_O) { sensor_O_ = sensor_O; }
+    void set_sensor_N(sensor::Sensor *sensor_N) { sensor_N_ = sensor_N; }
+    void set_sensor_ON(binary_sensor::BinarySensor *sensor_ON) { sensor_ON_ = sensor_ON; }
     void set_sda_pin(uint8_t sda_pin) { sda_pin_ = sda_pin; }
     void set_scl_pin(uint8_t scl_pin) { scl_pin_ = scl_pin; }
     void set_address(uint16_t address) { address_ = address; }
+    void set_on_pin(GPIOPin *on_pin) { this->on_pin_ = on_pin; }
 
     void setup() override;
     void loop() override;
@@ -33,9 +38,12 @@ class MPPT3000PRO : public sensor::Sensor, public PollingComponent {
     sensor::Sensor *sensor_D_;
     sensor::Sensor *sensor_T_;
     sensor::Sensor *sensor_O_;
+    sensor::Sensor *sensor_N_;
+    binary_sensor::BinarySensor *sensor_ON_;
     uint8_t sda_pin_;
     uint8_t scl_pin_;
     uint16_t address_;
+    GPIOPin *on_pin_{nullptr};
 };
 
 } //namespace mppt3000pro
