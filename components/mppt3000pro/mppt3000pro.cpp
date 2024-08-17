9e7af7f592fd7939a214db25efc23746ca2296d8
V0JT48 <46529036+V0JT48@users.noreply.github.com>
2024-08-17 18:22:19 +0200

    Added on state pin, parsing fixes

--- components/mppt3000pro/mppt3000pro.cpp
+++ components/mppt3000pro/mppt3000pro.cpp
@@ -1,5 +1,6 @@
 #include "esphome/core/log.h"
 #include "mppt3000pro.h"
+#include "MedianFilterLib.h"
 
 namespace esphome {
 namespace mppt3000pro {
@@ -22,27 +23,39 @@ uint16_t D;
 uint16_t T;
 uint8_t  O;
 uint16_t no_data_count = 0;
+bool on_state = false;
 
 uint16_t tmp_V = 0;
 uint16_t tmp_A = 0;
 uint16_t tmp_W = 0;
 uint16_t tmp_D = 0;
+//uint16_t chck_D = 0;
 uint16_t tmp_T = 0;
 uint8_t  tmp_O = DEFAULT_OUTPUT;
 
+MedianFilter<uint16_t> med_V(3);
+MedianFilter<uint16_t> med_A(3);
+MedianFilter<uint16_t> med_W(3);
+MedianFilter<uint16_t> med_D(3);
+MedianFilter<uint16_t> med_T(3);
+
 void send_data(void){
-  V = tmp_V;
-  A = tmp_A;
-  W = tmp_W;
-  D = tmp_D;
-  T = tmp_T;
+  V = med_V.AddValue(tmp_V);
+  A = med_A.AddValue(tmp_A);
+  W = med_W.AddValue(tmp_W);  
+  if (tmp_D >= D || tmp_D == 0){ // additional missed character value filter
+    D = med_D.AddValue(tmp_D);
+    //chck_D = tmp_D;
+  }
+  if (tmp_T >= T ){ // additional missed character value filter
+    T = med_T.AddValue(tmp_T);
+  }
   O = tmp_O;
-  new_data = true;
   ESP_LOGI("MPPT3000", "Received values: %dV %.1fA %dW D %.1fkWh T %dkWh Output %d", V, A/10.0, W, D/10.0, T, O);
 }
 
 void decode_character(void){
-  if (character > '.' && character <= 'z') { // printable char filter and skip decimal point for A and D
+  if (character > '.' && character <= 'z' && character != 'O') { // printable char filter and skip decimal point for A and D, skip O from ON OFF
     if((character >= '0') & (character <= '9')) {
         number = number*10 + (character - '0');
         prev_char_num = true;
@@ -50,18 +63,23 @@ void decode_character(void){
     else if(prev_char_num){
       switch (character){
         case 'V':
+          if (number > 400) break; // upper limit
           tmp_V=number; 
           break;
         case 'A':
+          if (number > 200) break; // upper limit
           tmp_A=number;
           break;
         case 'W':
+          if (number > 4000) break; // upper limit
           tmp_W=number;
           break;
         case 'k':
           switch(power) {
             case 'D':
+              if (number > 600) break; // upper limit
               tmp_D = number;
+              new_data = true; // mark data valid after D value is received to avoid mid day reset in case of node reboot
               break;
             case 'T':
               tmp_T = number;
@@ -73,8 +91,8 @@ void decode_character(void){
           }
           power = 'Y';
           break;
-        case 'O':
-          tmp_O=number;
+        case 'N':
+          if (number == 1 || number == 2) tmp_O=number; // ON, skipped OFF, -
           break;
       }
       number = 0;
@@ -93,7 +111,7 @@ void decode_character(void){
           break;
         case 'M':
           ESP_LOGI("MPPT3000", "Inverter Reboot");
-          break; 
+          break;
       }
       number = 0;
       prev_char_num = false;
@@ -107,7 +125,7 @@ void I2C_RxHandler(int numBytes)
   while(Wire.available()) {  // Read Any Received Data
     RxByte = Wire.read();
     switch(RxByte & 0x0F){
-      case 0x09:
+      case 0x09: // Data write, low E pin
         if(first_4bit){ // first 4bit, high bits
           character = RxByte & 0xF0;
           first_4bit = false;
@@ -137,6 +155,9 @@ void MPPT3000PRO::setup() {
   }
   Wire.begin(address_);
   Wire.onReceive(I2C_RxHandler);
+  if (this->on_pin_ != nullptr) {
+    this->on_pin_->setup();
+  }
   ESP_LOGI("MPPT3000", "Setup done");
 }
 
@@ -145,6 +166,10 @@ void MPPT3000PRO::loop() {
 }
 
 void MPPT3000PRO::update() {
+  if (this->sensor_ON_ != nullptr && this->on_pin_ != nullptr) {
+    on_state = this->on_pin_->digital_read();
+    this->sensor_ON_->publish_state(on_state);
+  }
   if(new_data){
     if (this->sensor_V_ != nullptr)
       this->sensor_V_->publish_state(V);
@@ -164,14 +189,24 @@ void MPPT3000PRO::update() {
   else {
     no_data_count++;
     ESP_LOGI("MPPT3000", "No new data %d time(s).", no_data_count);
+    if (no_data_count > 10 && on_state) {
+      Wire.end();
+      delay(30);
+      Wire.begin(address_);
+      Wire.onReceive(I2C_RxHandler);
+      no_data_count = 1;
+    }
   }
-
+  if (this->sensor_N_ != nullptr)
+    this->sensor_N_->publish_state(no_data_count);
 }
 
 void MPPT3000PRO::dump_config() {
     ESP_LOGCONFIG(TAG, "MPPT-3000PRO");
     ESP_LOGCONFIG(TAG, "  SDA Pin: GPIO%u", this->sda_pin_);
     ESP_LOGCONFIG(TAG, "  SCL Pin: GPIO%u", this->scl_pin_);
+    //ESP_LOGCONFIG(TAG, "  ON Pin: GPIO%u", this->on_pin_);
+    LOG_PIN("  ON Pin: ", this->on_pin_);
 }
 
 } //namespace mppt3000pro
